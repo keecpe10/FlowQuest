4dab016fe89295cd1493fc3aec4513770f72bf23
Panupong Donkrathok16 <panupongdonkrathok16@MacBook-Pro-khxng-Panupong.local>
2026-07-03 12:09:46 +0700

    Fix backend Werkzeug production error

--- backend/run.py
+++ backend/run.py
@@ -5,4 +5,4 @@ app = create_app()
 
 if __name__ == '__main__':
     debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() in ('true', '1', 't')
-    socketio.run(app, host='0.0.0.0', port=5001, debug=debug_mode)
+    socketio.run(app, host='0.0.0.0', port=5001, debug=debug_mode, allow_unsafe_werkzeug=True)
